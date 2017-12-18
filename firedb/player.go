105af3aa3c4c5420efc78e7fedd6ef88400e5224
sinmetal <metal.tie@gmail.com>
2017-12-19 00:01:40 +0900

    Watch Player Positionsのたたき台を作成 refs #3

---
+++ firedb/player.go
@@ -0,0 +1,86 @@
+package firedb
+
+import (
+	"context"
+	"sync"
+
+	"cloud.google.com/go/firestore"
+)
+
+var mu sync.RWMutex
+var db *firestore.Client
+
+// PlayerStore is PlayerStore
+type PlayerStore interface {
+	GetPlayerPositions(ctx context.Context) ([]*PlayerPosition, error)
+}
+
+// PlayerStoreImple is PlayerStoreImple
+type PlayerStoreImple struct{}
+
+var playerStore *PlayerStoreImple
+
+// NewPlayerStore is NewPlayerStore
+func NewPlayerStore() PlayerStore {
+	if playerStore != nil {
+		return playerStore
+	}
+	s := PlayerStoreImple{}
+	return &s
+}
+
+// SetPlayerStoreImple is 実装を差し替えたいときに利用する
+func SetPlayerStoreImple(s PlayerStoreImple) {
+	playerStore = &s
+}
+
+// PlayerPosition is Player Position Struct
+// TODO IDをstructの中に持つか、Mapで持つようにするか悩ましい
+type PlayerPosition struct {
+	ID     string  `firestore:"-" json:"id"`
+	Angle  float64 `json:"angle"`
+	IsMove bool    `json:"isMove"`
+	X      float64 `json:"x"`
+	Y      float64 `json:"y"`
+}
+
+// SetUp is SetUp
+func SetUp(ctx context.Context, projectID string) error {
+	if err := createWithSetClient(ctx, projectID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func createWithSetClient(ctx context.Context, projectID string) error {
+	client, err := firestore.NewClient(ctx, projectID)
+	if err != nil {
+		return err
+	}
+	mu.Lock()
+	defer mu.Unlock()
+	db = client
+
+	return nil
+}
+
+func (s *PlayerStoreImple) GetPlayerPositions(ctx context.Context) ([]*PlayerPosition, error) {
+	ds, err := db.Collection("world-default-player-position").Documents(ctx).GetAll()
+	if err != nil {
+		return nil, err
+	}
+
+	var pps []*PlayerPosition
+	for _, v := range ds {
+		var pp PlayerPosition
+		pp.ID = v.Ref.ID
+		err := v.DataTo(&pp)
+		if err != nil {
+			return nil, err
+		}
+		pps = append(pps, &pp)
+	}
+
+	return pps, nil
+}
--- main.go
+++ main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
+	"context"
+	"encoding/json"
 	"fmt"
 	"os"
+	"sync"
+	"time"
+
+	"github.com/sinmetal/slog"
+
+	"github.com/metal-tile/land/firedb"
 )
+
+var playerPositionMap *sync.Map
 
 func main() {
 	hs, err := os.Hostname()
@@ -13,4 +23,52 @@ func main() {
 	fmt.Printf("Hostname is %s\n", hs)
 	fmt.Println("")
 	fmt.Println(os.Environ())
+
+	playerPositionMap = &sync.Map{}
+
+	ctx := context.Background()
+	firedb.SetUp(ctx, "metal-tile-dev1")
+
+	ch := make(chan error)
+
+	go func() {
+		ch <- watchPlayerPositions()
+	}()
+
+	err = <-ch
+	fmt.Println(err.Error())
+}
+
+func watchPlayerPositions() error {
+	playerStore := firedb.NewPlayerStore()
+	for {
+		t := time.NewTicker(100 * time.Millisecond)
+		for {
+			select {
+			case <-t.C:
+				log := slog.Start(time.Now())
+				ctx := context.Background()
+				pps, err := playerStore.GetPlayerPositions(ctx)
+				if err != nil {
+					log.Errorf("playerStore.GetPlayerPositions. %s", err.Error())
+					log.Flush()
+					continue
+				}
+
+				// TODO playerStore.GetPlayerPositions の戻り値はmapの方が分かりやすい気がする
+				for _, v := range pps {
+					playerPositionMap.Store(v.ID, v)
+				}
+
+				// debug log
+				j, err := json.Marshal(pps)
+				if err != nil {
+					log.Errorf("json.Marshal. %s", err.Error())
+					log.Flush()
+				}
+				log.Infof(string(j))
+				log.Flush()
+			}
+		}
+	}
 }
